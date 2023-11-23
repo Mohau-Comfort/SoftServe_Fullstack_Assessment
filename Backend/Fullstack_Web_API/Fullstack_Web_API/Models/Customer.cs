531db6451576831d611ef457e3ace81997e77893
Mohau-Comfort <ledmo98@gmail.com>
2023-11-23 14:14:04 +0200

    added model class

--- Backend/Fullstack_Web_API/Fullstack_Web_API/Models/Customer.cs
+++ Backend/Fullstack_Web_API/Fullstack_Web_API/Models/Customer.cs
@@ -2,5 +2,31 @@
 {
     public class Customer
     {
+
+        public Guid CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName => $"{FirstName} {LastName}";
+        public string EmailAddress { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age => CalculateAge();
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateEdited { get; set; }
+        public bool IsDeleted { get; set; }
+
+        //function to calculate age automatically based on date of birth
+        private int CalculateAge()
+        {
+            DateTime currentDate = DateTime.Now;
+            int age = currentDate.Year - DateOfBirth.Year;
+
+            if (currentDate < DateOfBirth.AddYears(age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+
     }
 }
